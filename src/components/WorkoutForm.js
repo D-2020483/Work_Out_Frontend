ca278d0ab93ba29bd5d23e56ae79e9c06e4dc77d
Dinithi Imalsha Weerasingha <is2020483@mgt.rjt.ac.lk>
2025-02-22 12:33:15 +0530

    Changes

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -15,7 +15,7 @@ const WorkoutForm = () => {
 
         const workout = {title, load, reps}
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch('https://fed-workout-buddy-dinithi.onrender.com/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
